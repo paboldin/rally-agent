3d84cbc41ed2df3dc3c920418c8014ed314fd5bf
Pavel Boldin <boldin.pavel@gmail.com>
2015-09-22 03:47:07 +0300

    updated version

--- agent.py
+++ agent.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+import time
 import sys
 import zmq
+import subprocess
+import uuid
 
 subscribe_context = zmq.Context()
 subscribe_socket = subscribe_context.socket(zmq.SUB)
@@ -12,10 +15,24 @@ push_context = zmq.Context()
 push_socket = push_context.socket(zmq.PUSH)
 push_socket.connect("tcp://127.0.0.1:1235")
 
+AGENT = str(uuid.uuid4())
 while True:
-        print "SUB:"
         d = subscribe_socket.recv_json()
-        print d
-        d[sys.argv[1]] = sys.argv[2]
+        r = {
+            "req": d["req"],
+            "agent": AGENT
+        }
+        action = d.get("action")
+        if action == "ping":
+            pass
+        elif action == "command":
+            stdout, stderr = subprocess.Popen(
+                " ".join(d["path"]), shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            ).communicate()
+            d["stdout"] = stdout
+            d["stderr"] = stderr
+        else:
+            d["error"] = "Action %s unknown" % action
+
         push_socket.send_json(d)
-        print "PUSHED"
--- masteragent.py
+++ masteragent.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import cgi
+import collections
+import datetime
 import json
 import six
 import threading
@@ -22,17 +24,87 @@ pull_socket = pull_context.socket(zmq.PULL)
 pull_socket.bind("tcp://*:1235")
 
 req_queue = queue.Queue()
+missed_queue = collections.defaultdict(list)
+
 
 def recv_thread(req_queue):
     while True:
-        queue = req_queue.get()
-        if queue is None:
+        req, conf, queue = req_queue.get()
+        if req is None:
             return
-        while pull_socket.poll(1000):
-            queue.append(pull_socket.recv_json())
+        timeout = conf["timeout"]
+        tstart = datetime.datetime.now()
+        if queue is not None:
+            queue.extend(missed_queue.pop(req, []))
+        while pull_socket.poll(timeout) and \
+                len(queue) <= conf["agents"]:
+            resp = pull_socket.recv_json()
+            if resp["req"] != req:
+                missed_queue[resp["req"]].append(resp)
+            else:
+                queue.append(resp)
+            timeout = max(
+                timeout -
+                    (datetime.datetime.now() - tstart).total_seconds() * 1000,
+                0)
         req_queue.task_done()
 
+
+def make_flat(form):
+    d = {}
+    for k in form.keys():
+        if isinstance(form[k], list):
+            d[k] = [x.value for x in form[k]]
+        else:
+            d[k] = form[k].value
+    return d
+
+
 class PostHandler(six.moves.BaseHTTPServer.BaseHTTPRequestHandler):
+    config = dict(timeout=1000, agents=float("+inf"))
+
+    def configure(self, query):
+        config = dict(six.moves.urllib.parse.parse_qsl(query))
+        self.config["timeout"] = float(
+            config.get("timeout", self.config["timeout"])
+        )
+        self.config["agents"] = float(
+            config.get("agents", self.config["agents"])
+        )
+        self.send_response(200)
+
+    def missed(self):
+        req_queue.put(("", {"timeout": 10000, "agents": float('+Inf')}, []))
+        req_queue.join()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("Missed: %s\n" % json.dumps(missed_queue))
+        if self.command == "DELETE":
+            missed_queue.clear()
+
+    def do_PUT(self):
+
+        url = six.moves.urllib.parse.urlparse(self.path)
+        if url.path == "/configure":
+            return self.configure(url.query)
+
+        self.send_response(404)
+
+    def do_GET(self):
+        url = six.moves.urllib.parse.urlparse(self.path)
+        if url.path == "/missed":
+            return self.missed()
+
+        self.send_response(404)
+
+    def do_DELETE(self):
+        url = six.moves.urllib.parse.urlparse(self.path)
+        if url.path == "/missed":
+            return self.missed()
+        
+        self.send_response(404)
+
     def do_POST(self):
         form = cgi.FieldStorage(
             fp=self.rfile,
@@ -43,13 +115,14 @@ class PostHandler(six.moves.BaseHTTPServer.BaseHTTPRequestHandler):
             }
         )
 
-        print self.path
+        req = str(uuid.uuid4())
 
-        publish_socket.send_json(
-            {k: form[k].value for k in form.keys()})
+        d = make_flat(form)
+        d["req"] = req
+        publish_socket.send_json(d)
 
         r = []
-        req_queue.put(r)
+        req_queue.put((req, self.config, r))
         req_queue.join()
 
         self.send_response(200)
@@ -65,5 +138,4 @@ if __name__ == "__main__":
     try:
         server.serve_forever()
     except KeyboardInterrupt:
-        req_queue.put(None)
-        pass
+        req_queue.put((None, None, None))
