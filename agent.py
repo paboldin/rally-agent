6b372a322b23e493cc8b6ec8008e9c02ca9d5519
Pavel Boldin <boldin.pavel@gmail.com>
2015-09-22 04:44:48 +0300

    fix stdout/stderr file reading

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ import sys
 import zmq
 import subprocess
 import uuid
+import tempfile
 
 subscribe_context = zmq.Context()
 subscribe_socket = subscribe_context.socket(zmq.SUB)
@@ -18,6 +19,8 @@ push_socket.connect("tcp://127.0.0.1:1235")
 AGENT = str(uuid.uuid4())
 while True:
         d = subscribe_socket.recv_json()
+        if d.get("target", AGENT) != AGENT:
+            continue
         r = {
             "req": d["req"],
             "agent": AGENT
@@ -26,13 +29,35 @@ while True:
         if action == "ping":
             pass
         elif action == "command":
+            stdout_fh = subprocess.PIPE
+            stderr_fh = subprocess.PIPE
+            if d.get("stdout_log"):
+                stdout_fh = tempfile.NamedTemporaryFile(delete=False)
+            if d.get("stderr_log"):
+                stderr_fh = tempfile.NamedTemporaryFile(delete=False)
             stdout, stderr = subprocess.Popen(
-                " ".join(d["path"]), shell=True,
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                " ".join(d["path"]),
+                shell=True,
+                stdout=stdout_fh,
+                stderr=stderr_fh
             ).communicate()
-            d["stdout"] = stdout
-            d["stderr"] = stderr
+            if stdout is not None:
+                r["stdout"] = stdout
+            else:
+                r["stdout_fh"] = stdout_fh.name
+            if stderr is not None:
+                r["stderr"] = stderr
+            else:
+                r["stderr_fh"] = stderr_fh.name
+        elif action == "read":
+            with file(d["filename"]) as fh:
+                fh.seek(int(d.get("seek", 0)))
+                if "size" in d:
+                    content = fh.read(int(d["size"]))
+                else:
+                    content = fh.read()
+                r["content"] = content
         else:
-            d["error"] = "Action %s unknown" % action
+            r["error"] = "Action %s unknown" % action
 
-        push_socket.send_json(d)
+        push_socket.send_json(r)
